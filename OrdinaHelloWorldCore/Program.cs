d6055669f16f59519bce9f67f405a560757fbc21
stefanvt1981 <stefanvantilborg@hotmail.com>
2019-01-18 16:06:55 +0100

    Add project files.

---
+++ OrdinaHelloWorldCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OrdinaHelloWorldCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine(@"       Core Edition       ==     ");
+            Console.Out.WriteLine(@"         ## ## ##        ==     ");
+            Console.Out.WriteLine(@"      ## ## ## ## ##    ===     ");
+            Console.Out.WriteLine(" / \"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\\___/ ===   ");
+            Console.Out.WriteLine(@"{                       /  ===- ");
+            Console.Out.WriteLine(@" \______ O           __ /       ");
+            Console.Out.WriteLine(@"  \    \         __ /           ");
+            Console.Out.WriteLine(@"   \____\_______ /              ");
+            Console.Out.WriteLine();
+            Console.Out.WriteLine("Hello Ordina Docker Workshop Deelnemers!");
+        }
+    }
+}
